0b6c4b32439dbfc4998b8f9ab4260d46bf6877ff
gnozell <gnozell@yahoo.com>
2016-05-16 07:10:30 -0400

    more paper and movement

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform target;
+	public float dampTime = 0.15f;
+	public float lowerLimit = -4f;
+
+
+	private string phase = "followPlayer";
+	private Camera thiscam;
+	private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		thiscam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (phase) {
+
+		case "followPlayer":{
+				if (target)
+				{
+					Vector3 point = thiscam.WorldToViewportPoint(target.position);
+					Vector3 delta = target.position - thiscam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 20));
+					Vector3 destination = transform.position + delta;
+
+					if (destination.y < lowerLimit) {
+						destination.y = lowerLimit;
+					}
+
+					// moves the camera
+					transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+				}
+				break;
+			}
+
+		case "freeze":
+			{
+				
+				break;
+			}
+
+		default:
+			{
+				phase = "followPlayer";
+				break;
+			}
+		}
+	
+	}
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	Rigidbody playerRb;
+	private float speed = 10f;
+	public float cSpeed;
+	public bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+		playerRb = GetComponent<Rigidbody> ();
+		cSpeed = speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hor = Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+
+		playerRb.AddForce (new Vector3 (hor * cSpeed,0, vert * cSpeed));
+
+		// limits max speed
+		if (playerRb.velocity.magnitude > speed) {
+			playerRb.velocity = Vector3.ClampMagnitude(playerRb.velocity, cSpeed);
+		}
+
+		// turning the sprite bool
+		if (hor > 0) {
+			facingRight = true;
+		} else if (hor < 0) {
+			facingRight = false;
+		}
+
+		// action of turning
+		if (facingRight){
+			if (transform.rotation.eulerAngles.y > 0) {
+				// turns to 0
+				transform.Rotate(new Vector3(0,-5,0));
+			}
+		}else{
+			if (transform.rotation.eulerAngles.y < 180) {
+				// turns to 180
+				transform.Rotate(new Vector3(0,5,0));
+			}
+		}
+
+		// Jumping
+		if (Input.GetButtonDown ("Jump")) {
+			playerRb.AddForce (new Vector3 (0,cSpeed*100, 0));
+		}
+	
+	}
+}
